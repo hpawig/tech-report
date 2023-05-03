ce639a45f6ea0e63da9ef8d67f14fe2c8871b1a3
hpawig <pawighannah@gmail.com>
2023-05-03 10:01:14 -0700

    running prop test on data

--- scripts/reading_raw_data.R
+++ scripts/reading_raw_data.R
@@ -2,7 +2,7 @@
 library(here)
 library(readxl)
 
-#read in data
+#read in pong excel sheet
 pong <- read_excel(here::here('data',
                               'data_raw',
                               'raw pong data.xlsx'))
---
+++ scripts/two_prop_test.R
@@ -0,0 +1,48 @@
+# This script will show
+# My process of running a 2 proportion test
+
+# loading in packages
+library(tidyverse)
+library(forcats)
+
+# let pi_w = proportion of success for wet method
+# pi_d = proportion of success for dry method
+
+# creating analysis data set from raw data
+pong_test <- pong
+
+# create a column of trial IDs in the test data
+pong_test$trial <- c(1:100)
+
+# cleaning / re-coding data
+pong_test <- pong_test |> 
+  select(trial, method, success, which_cup) |> 
+  mutate(method = as.factor(method),
+       which_cup = as.factor(which_cup),
+       which_cup = fct_recode(.f = which_cup,
+                               None = "0"))
+
+
+
+
+# finding number of successes and failures for each method
+prop.table <- pong_test |> 
+  group_by(method) |> 
+  summarise(accuracy = mean(success)) #taking the mean of a dummy variable gives a prop
+
+# 50 trials were run for each method
+num_shots <- c(50,50)
+
+# storing the proportions
+p_dry <- 0.38
+p_wet <- 0.38
+
+#vector of both proportions 
+two_props <- c(p_dry, p_wet)
+
+
+
+# running a two-sided 2 proportion test
+prop.test(x = two_props, n = num_shots, 
+          alternative='two.sided', conf.level = 0.95)
+
