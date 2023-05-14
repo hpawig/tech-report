b94e7e8675f971100112b68e6afa664421fd39e5
hpawig <pawighannah@gmail.com>
2023-05-14 16:13:07 -0700

    fixing figure titles and rendering 1.4 draft

--- scripts/figure3_barplot2.R
+++ scripts/figure3_barplot2.R
@@ -5,50 +5,46 @@ library(ggtext)
 # make sure to read in data from the file, reading_raw_data.R 
 # then run the R script two_prop_test.R 
 
-#### Figure 3 ####
-# get proportions per method
+#### Figure 2 ####
+# getting total proportion of success and failure
+pong_total_counts <- pong_test |> 
+  ungroup() |> 
+  select(method, success) |> 
+  mutate(method = "Combined",
+         shot_made = case_when(
+           success == 1 ~ "Yes",
+           success == 0 ~ "No"))
 
-pong_overall_counts <- pong_test |>
+pong_counts <- pong_test |> 
   mutate(
     method = fct_recode(.f = method,
                         "Dry" = "D",
                         "Wet" = "W"),
-    
-    success_by_method = case_when(
-      success == 1 & method == "Dry" ~ "Success, Dry",
-      success == 1 & method == "Wet" ~ "Success, Wet",
-      TRUE ~ "Failure"),
-    
-    success_by_method = factor(success_by_method, levels = c("Failure",
-                                                             "Success, Wet",
-                                                             "Success, Dry")),
-    overall = "Overall")
+    shot_made = case_when(
+      success == 1 ~ "Yes",
+      success == 0 ~ "No")) |> 
+  group_by(shot_made, method)
+
+# create the plot
+
+pong_counts |>
+  mutate(
+    shot_made = fct_recode(.f = shot_made,
+                           "Missed Shot" = "No",
+                           "Attempt Successful" = "Yes")) |> 
+  ggplot(mapping = aes(x = method,
+                       fill = shot_made)) +
+  geom_bar(stat = "count") +
+  labs(title = "Figure 3: Breakdown of Shot Attempts",
+       subtitle = "For Each Playing Method",
+       x = "Playing Method") +
+  scale_fill_manual(guide_legend(title = "Attempt Status"),
+                    values = c('firebrick','salmon')) +
+  theme(axis.title.y = element_blank(),
+        axis.title.x = element_text(size = 8))
+
+ggsave("barplot_by_method.png", path = "/Users/hannahpawig/Desktop/STAT 365 Report/tech_report/results")
 
 
-# making the plot
-pong_overall_counts |> 
-  ggplot(mapping = aes(x = overall,
-                       fill = success_by_method)) + 
-  geom_bar(stat = "count",
-           position = "stack") +
-  labs(title = "Overall Successes of the 100 Shot Attempts",
-       subtitle = "Successes with the 
-       <span style='color:darkgreen'>Dunk <span style='color:darkgreen'>Method 
-       <span style='color:black'>and
-        <span style='color:limegreen'>Dry <span style='color:limegreen'>Ball Method") +
-  theme(axis.title = element_blank(),
-        axis.text.x = element_blank(),
-        axis.ticks.x = element_blank(),
-        plot.subtitle = element_markdown(),
-        legend.position = "none") +
-  scale_fill_manual(values = c('gray48', 'darkgreen','limegreen')) +
-  annotate("text", x = .75, y = 36, 
-           label = "Total Successes (38)",
-           color = "white", size = 3) +
-  annotate("text", x = 1.35, y = 98, color = 'lightgrey', size = 3, label = "Failures") +
-  scale_y_continuous(breaks = c(0,25,38,50,75,100))
 
 
-# saving plot as png
-ggsave(filename = "barplot_overall_success.png",
-       path = "/Users/hannahpawig/Desktop/STAT 365 Report/tech_report/results")
